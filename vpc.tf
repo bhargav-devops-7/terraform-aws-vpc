2dd26cc741eebd78586a95eb02a141a4ec91f6aa
bhargav-devops-7 <bhargavdodda996@gmail.com>
2026-03-17 21:15:54 +0530

    terraform-vpc-module

--- vpc.tf
+++ vpc.tf
@@ -155,7 +155,7 @@ resource "aws_nat_gateway" "nat" {
 
   # To ensure proper ordering, it is recommended to add an explicit dependency
   # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.main]
+  depends_on = [aws_internet_gateway.igw]
 }
 
 # 7. Create Routes
