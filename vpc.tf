9a9f6a59b7cd10d8d1601b6e704ed350d08ca398
bhargav-devops-7 <bhargavdodda996@gmail.com>
2026-03-17 21:10:29 +0530

    terraform-vpc-module

--- vpc.tf
+++ vpc.tf
@@ -35,7 +35,7 @@ resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet_cidrs[count.index]
-  availbility_zone = local.az_names[count.index]
+  availability_zone = local.az_names[count.index]
   map_public_ip_on_launch = true
 
   tags = merge(
@@ -52,7 +52,7 @@ resource "aws_subnet" "private" {
   count = length(var.private_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.private_subnet_cidrs[count.index]
-  availbility_zone = local.az_names[count.index]
+  availability_zone = local.az_names[count.index]
 
   tags = merge(
     var.private_subnet_tags,
@@ -68,7 +68,7 @@ resource "aws_subnet" "database" {
   count = length(var.database_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.database_subnet_cidrs[count.index]
-  availbility_zone = local.az_names[count.index]
+  availability_zone = local.az_names[count.index]
 
   tags = merge(
     var.database_subnet_tags,
