2dc95fe49735f66b1363958bf56d06b867c9b06f
bhargav-devops-7 <bhargavdodda996@gmail.com>
2026-03-18 16:55:00 +0530

    terraform-vpc-module

--- peering.tf
+++ peering.tf
@@ -3,7 +3,7 @@
 resource "aws_vpc_peering_connection" "default" {
   count = var.is_peering_connection_required ? 1 : 0
 
-  peer_vpc_id   = aws_vpc.default.id
+  peer_vpc_id   = data.aws_vpc.default.id
   vpc_id        = aws_vpc.main.id
 
   accepter {
